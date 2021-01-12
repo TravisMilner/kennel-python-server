48ea3e4a9673dd3e5928880261bc369b465e7e64
Travis Milner <travismilner1@gmail.com>
2021-01-12 13:26:05 -0600

    added post request methods to customers, employees, animals, locations

--- request_handler.py
+++ request_handler.py
@@ -1,12 +1,12 @@
+import json 
 from customers.request import get_single_customer
 from employees.request import get_all_employees, get_single_employee
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from locations.request import get_all_locations, get_single_location
 from animals import get_all_animals, get_single_animal, create_animal
-from locations import get_all_locations, get_single_location
-from employees import get_all_employees, get_single_employee
-from customers import get_all_customers, get_single_customer
-import json 
+from locations import get_all_locations, get_single_location, create_location
+from employees import get_all_employees, get_single_employee, create_employee
+from customers import get_all_customers, get_single_customer, create_customer
 
 
 # Here's a class. It inherits from another class.
@@ -103,16 +103,26 @@ class HandleRequests(BaseHTTPRequestHandler):
         (resource, id) = self.parse_url(self.path)
 
         # Initialize new animal
-        new_animal = None
+        new_entry =  None
 
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
         # function next.
         if resource == "animals":
-            new_animal = create_animal(post_body)
+            new_entry = create_animal(post_body)
+
+        if resource == "locations":
+            new_entry = create_location(post_body)
+
+        if resource == "employees":
+            new_entry = create_employee(post_body)
+
+        if resource == "customers":
+            new_entry = create_customer(post_body)
 
         # Encode the new animal and send in response
-        self.wfile.write(f"{new_animal}".encode())
+        self.wfile.write(f"{new_entry}".encode())
+        
 
 
     # Here's a method on the class that overrides the parent's method.
